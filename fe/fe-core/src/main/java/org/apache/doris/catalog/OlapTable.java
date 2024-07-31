caa9774f9e03053a98806a7eaa6ce6ecaf681b0d
abmdocrt <Yukang.Lian2022@gmail.com>
2024-07-31 22:07:08 +0800

    [Fix](delete command) Mark delete sign when do delete command in MoW table #35917 (#38468)

--- fe/fe-core/src/main/java/org/apache/doris/catalog/OlapTable.java
+++ fe/fe-core/src/main/java/org/apache/doris/catalog/OlapTable.java
@@ -1128,6 +1128,14 @@ public class OlapTable extends Table {
         return null;
     }
 
+    public void setEnableMowLightDelete(boolean enable) {
+        getOrCreatTableProperty().setEnableMowLightDelete(enable);
+    }
+
+    public boolean getEnableMowLightDelete() {
+        return getOrCreatTableProperty().getEnableMowLightDelete();
+    }
+
     public Boolean hasSequenceCol() {
         return getSequenceCol() != null;
     }
