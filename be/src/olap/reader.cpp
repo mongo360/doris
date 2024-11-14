0cef3acb52da576cde0159e161120040cc209010
wangqingtao6 <wangqingtao5@jd.com>
2024-11-14 11:13:03 +0800

    light_schema_change_support_add_key_column

--- be/src/olap/reader.cpp
+++ be/src/olap/reader.cpp
@@ -523,9 +523,9 @@ Status TabletReader::_init_conditions_param(const ReaderParams& read_params) {
     for (const auto& filter : read_params.function_filters) {
         _col_predicates.emplace_back(_parse_to_predicate(filter));
         auto* pred = _col_predicates.back();
-        const auto& col = _tablet->tablet_schema()->column(pred->column_id());
+        const auto& col = _tablet_schema->column(pred->column_id());
         auto is_like = is_like_predicate(pred);
-        auto* tablet_index = _tablet->tablet_schema()->get_ngram_bf_index(col.unique_id());
+        auto* tablet_index = _tablet_schema->get_ngram_bf_index(col.unique_id());
 
         if (is_like && tablet_index && config::enable_query_like_bloom_filter) {
             std::unique_ptr<segment_v2::BloomFilter> ng_bf;
